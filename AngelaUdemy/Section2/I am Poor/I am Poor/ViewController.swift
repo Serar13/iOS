674cc9419b94002861c64141691565ee5fcee604
Rares Simon <raressimon@Raress-MacBook-Pro.local>
2024-09-29 19:15:02 +0300

    Add sections to AngelaUdemy folder

---
+++ AngelaUdemy/Section2/I am Poor/I am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Poor
+//
+//  Created by Rares Simon on 22.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
